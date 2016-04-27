64bbef76925eac0155b04a896594e484d7ffafa6
Alex Dolski <alexd@n-ary.net>
2016-04-27 10:56:38 -0500

    Minor refactoring and improve documentation

--- collections/archives/process.rb
+++ collections/archives/process.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Traverses an arbitrary file/folder hierarchy and creates LRP AIP XML files
+# Traverses an arbitrary file/folder hierarchy and creates DLS XML files
 # from its contents.
 #
 # The output folder structure is the same as the source structure.
@@ -23,7 +23,7 @@ end
 
 source_pathname = File.expand_path(source_pathname)
 unless File.exist?(source_pathname)
-  puts "Source pathname does not exist."
+  puts 'Source pathname does not exist.'
   exit
 end
 unless File.directory?(source_pathname)
@@ -36,24 +36,31 @@ def relative_pathname(source_pathname, pathname)
   pathname.reverse.chomp(source_pathname.reverse).reverse
 end
 
-def encoded_id(collection_id, source_pathname, pathname)
-  Digest::MD5.hexdigest(collection_id + unencoded_id(source_pathname, pathname))
+def encoded_id(collection_id, unencoded_id)
+  # The collection ID adds some "salt," preventing collisions in the case of
+  # the same unencoded ID (relative pathname) appearing in multiple
+  # collections.
+  Digest::MD5.hexdigest(collection_id + unencoded_id)
 end
 
-def unencoded_id(source_pathname, pathname)
-  relative_pathname(source_pathname, pathname).reverse.chomp('/').reverse
+##
+# @param root_pathname [String] Root pathname of the collection
+# @param pathname[String] Pathname to generate an ID for
+# @return [String] Pathname of the given pathname within the root collection
+#                  pathname with no leading slash
+#
+def unencoded_id(root_pathname, pathname)
+  relative_pathname(root_pathname, pathname).reverse.chomp('/').reverse
 end
 
 Dir.glob(source_pathname + '/**/*').each do |pathname|
-
   # Skip junk files
   next if %w(Thumbs.db .DS_Store).include?(File.basename(pathname))
 
   # Skip empty folders
   next if File.directory?(pathname) and Dir.glob(pathname + '/*').length < 1
 
-  # Replace . and / with -
-  object_id = encoded_id(collection_id, source_pathname, pathname)
+  object_id = encoded_id(collection_id, unencoded_id(source_pathname, pathname))
 
   builder = Nokogiri::XML::Builder.new do |xml|
     xml['lrp'].Object("xmlns:lrp" => "http://www.library.illinois.edu/lrp/terms#") {
@@ -90,7 +97,8 @@ Dir.glob(source_pathname + '/**/*').each do |pathname|
       parent_path = File.expand_path(pathname + '/..')
       if parent_path.length > source_pathname.length
         xml['lrp'].parentId {
-          xml.text(encoded_id(collection_id, source_pathname, parent_path))
+          xml.text(encoded_id(collection_id,
+              unencoded_id(source_pathname, parent_path)))
         }
       end
 
